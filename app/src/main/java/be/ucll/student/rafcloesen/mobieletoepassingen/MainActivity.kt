598834534357edaa760efed82a5722f6aed8a136
Raf Cloesen <raf.cloesen@student.ucll.be>
2021-07-16 20:45:12 +0200

    Add first skeleton.
    Add title & scroll view. Add one dummy item onCreate.

--- app/src/main/java/be/ucll/student/rafcloesen/mobieletoepassingen/MainActivity.kt
+++ app/src/main/java/be/ucll/student/rafcloesen/mobieletoepassingen/MainActivity.kt
@@ -2,10 +2,16 @@ package be.ucll.student.rafcloesen.mobieletoepassingen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val todoList = findViewById<LinearLayout>(R.id.todoList)
+        val item = TextView(this)
+        item.text = "Hello World!"
+        todoList.addView(item)
     }
 }
